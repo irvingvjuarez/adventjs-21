a9312acc6e53d702a7181a3fc65c0fda0ae6df4b
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-02 23:09:50 -0500

    First exercise tests passed correctly

---
+++ src/one/countSheep.test.js
@@ -0,0 +1,97 @@
+import { describe, it, expect } from "vitest";
+
+function countSheep(sheep) {
+	if (!Array.isArray(sheep)) {
+		throw new Error()
+	}
+
+	const chosenColor = "rojo"
+	const availableLetters = ["n", "a"]
+
+	return sheep.filter(item => {
+		return item.color === chosenColor &&
+			item.name.toLowerCase().includes(...availableLetters)
+	});
+}
+
+describe("Count sheep", () => {
+	// it("countSheep should be a function", () => {
+	// 	expect(countSheep).toBeTypeOf("function")
+	// })
+
+	it("Should receive a parameter", () => {
+		expect(() => countSheep()).toThrow()
+	})
+
+	it("Should receive an array as parameter", () => {
+		expect(() => countSheep("a")).toThrow()
+		expect(() => countSheep({})).toThrow()
+		expect(() => countSheep(0)).toThrow()
+	})
+
+	it("Should return an array as answer", () => {
+		expect(countSheep([])).toBeInstanceOf(Array)
+	})
+
+	// it("Should return an array of objects having the property color: 'rojo'", () => {
+	// 	const arr = [
+	// 		{color: "azul", color: "amarillo", color: "verde"}
+	// 	]
+	// 	expect(countSheep(arr)).toStrictEqual([])
+
+	// 	const arr2 = [
+	// 		{color: "azul", color: "amarillo", color: "verde", color: "rojo"}
+	// 	]
+	// 	expect(countSheep(arr2)).toStrictEqual([ {color: "rojo"} ])
+	// })
+
+	it("Should return an array of object having property color: 'rojo' and name containing letters n and a", () => {
+		const arr = [
+			{
+				color: "azul",
+				name: "jul"
+			},
+			{
+				color: "verde",
+				name: "qyo"
+			},
+			{
+				color: "rojo",
+				name: "pñlko"
+			}
+		]
+		expect(countSheep(arr)).toStrictEqual([])
+
+		const arr2 = [
+			{
+				color: "rojo",
+				name: "na"
+			},
+			{
+				color: "rojo",
+				name: "pñlko"
+			}
+		]
+		expect(countSheep(arr2)).toStrictEqual([{
+			color: "rojo",
+			name: "na"
+		}])
+	})
+
+	it("Should return items where property name includes both letters n and a, regardless of capital letters", () => {
+		const arr2 = [
+			{
+				color: "rojo",
+				name: "NA"
+			},
+			{
+				color: "rojo",
+				name: "pñlko"
+			}
+		]
+		expect(countSheep(arr2)).toStrictEqual([{
+			color: "rojo",
+			name: "NA"
+		}])
+	})
+})

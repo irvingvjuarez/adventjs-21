48d8f6d25bab12196fd2e809f4df5271bd839a13
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-03 23:22:14 -0500

    Refactoring the second exercise

--- src/two/listGifts.test.js
+++ src/two/listGifts.test.js
@@ -1,32 +1,5 @@
 import { describe, expect, it } from "vitest";
-
-const listGifts = (letter) => {
-	let errorMsg = ""
-	if (!letter) {
-		errorMsg = "A parameter must be passed"
-	} else if (typeof letter !== "string") {
-		errorMsg = "A string should be passed as parameter"
-	}
-
-	if (errorMsg !== "") throw new Error(errorMsg)
-
-	const res = {}
-
-	letter = letter.split(" ")
-		.filter(item => item !== "")
-		.map(item => item.trim())
-
-	const uniqueValues = [...new Set(letter)]
-
-	uniqueValues.forEach(value => {
-		if (value.charAt(0) !== "_") {
-			const size = letter.filter(item => item == value).length
-			res[value] = size
-		}
-	})
-
-	return res
-}
+import { listGifts } from "./listGifts"
 
 describe("List Gifts algorithm", () => {
 	// it("listGifts should be a function", () => {

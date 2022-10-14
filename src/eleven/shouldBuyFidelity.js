6c7816a76e1e4c23e34639144ddd403a53025a6f
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-14 12:10:24 -0500

    Super refactor

--- src/eleven/shouldBuyFidelity.js
+++ src/eleven/shouldBuyFidelity.js
@@ -1,15 +1,5 @@
-const ticketPrice = 12
-
 export const shouldBuyFidelity = (times) => {
 	if (!times || typeof times !== "number") throw new Error()
 
-	const withoutFidelity = ticketPrice * times
-	let fidelity = 250
-
-	for (let i = 1; i <= times; i++) {
-		const relativePrice = ticketPrice * (0.75 ** i)
-		fidelity += relativePrice
-	}
-
-	return fidelity < withoutFidelity
+	return times >= 24
 }

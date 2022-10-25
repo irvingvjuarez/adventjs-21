3acf07e09a25eaf3baecb726269257194dad8ed0
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-24 20:25:17 -0500

    Twentieth exercise passed all tests correctly

--- src/twenty/pangram.test.js
+++ src/twenty/pangram.test.js
@@ -2,6 +2,16 @@ import { describe, expect, it } from "vitest";
 
 const pangram = (letter) => {
 	if (!letter || typeof letter !== "string") throw new Error()
+
+	const alphabet = "abcdefghijklmnñopqrstuvwxyz".split("")
+
+	const cleanedLetter = letter.split("")
+		.filter(char => char !== " ") // remove blanks
+		.map(char => char.toLowerCase()) // to lower case
+
+	const hasAllCharacters = alphabet.every(char => cleanedLetter.includes(char))
+
+	return hasAllCharacters
 }
 
 describe("pangram tests", () => {
@@ -19,5 +29,19 @@ describe("pangram tests", () => {
 		expect(() => pangram(Function)).toThrow()
 		expect(() => pangram(Object)).toThrow()
 		expect(() => pangram(Array)).toThrow()
+	})
+
+	it("Should return a boolean", () => {
+		expect(pangram("a")).toBeTypeOf("boolean")
+	})
+
+	it("Should return true if the string has all the alphabet letters", () => {
+		expect(pangram("Extraño pan de col y kiwi se quemó bajo fugaz vaho")).toBe(true)
+		expect(pangram('Jovencillo emponzoñado y con walkman: ¡qué figurota exhibes!')).toBe(true)
+	})
+
+	it("Should be able to analyze if the letter doesn't have all the alphabet letters", () => {
+		expect(pangram('Esto es una frase larga pero no tiene todas las letras del abecedario')).toBe(false)
+		expect(pangram('De la a a la z, nos faltan letras')).toBe(false)
 	})
 })

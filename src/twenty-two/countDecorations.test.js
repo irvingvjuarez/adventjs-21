3a2f07e551d449921531e62b964a4e8ab2b8e23a
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-26 18:20:46 -0500

    Refactor in twenty-second exercise to valid all the tests again

--- src/twenty-two/countDecorations.test.js
+++ src/twenty-two/countDecorations.test.js
@@ -1,21 +1,21 @@
 import { describe, it, expect } from "vitest";
 
 const countDecorations = (tree) => {
-	// const isInvalid = !tree
-	// 	|| tree instanceof Object === false
-	// 	|| typeof tree !== "object"
+	const isInvalid = !tree
+		|| tree instanceof Object === false
+		|| typeof tree !== "object"
 
-	// if (isInvalid) throw new Error()
+	if (isInvalid) throw new Error()
 	let decorations = 0
-	if (!tree) return decorations
 
 	const branchesNames = Object.keys(tree)
 
 	for(let branchName of branchesNames) {
 		const branchValue = tree[branchName]
-		if (typeof branchValue === "number") {
+		const branchType = typeof branchValue
+		if (branchType === "number") {
 			decorations += branchValue
-		} else {
+		} else if (branchValue && typeof branchValue === "object") {
 			decorations += countDecorations(branchValue)
 		}
 	}
@@ -32,13 +32,13 @@ describe("countDecorations", () => {
 	// 	expect(() => countDecorations()).toThrow()
 	// })
 
-	// it("Should throw error if the first and only parameter is not and object", () => {
-	// 	expect(() => countDecorations(String)).toThrow()
-	// 	expect(() => countDecorations(Number)).toThrow()
-	// 	expect(() => countDecorations(Boolean)).toThrow()
-	// 	expect(() => countDecorations(Function)).toThrow()
-	// 	expect(() => countDecorations(Array)).toThrow()
-	// })
+	it("Should throw error if the first and only parameter is not and object", () => {
+		expect(() => countDecorations(String)).toThrow()
+		expect(() => countDecorations(Number)).toThrow()
+		expect(() => countDecorations(Boolean)).toThrow()
+		expect(() => countDecorations(Function)).toThrow()
+		expect(() => countDecorations(Array)).toThrow()
+	})
 
 	it("Should return a number", () => {
 		expect(countDecorations({})).toBeTypeOf("number")

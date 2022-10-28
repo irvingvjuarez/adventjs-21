00d3386ef5047066acef988b57de7e42d2f8d933
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-28 14:21:02 -0500

    Code passed all tests (twenty-four)

--- src/twenty-four/checkIsSameTree.test.js
+++ src/twenty-four/checkIsSameTree.test.js
@@ -1,5 +1,29 @@
 import { describe, expect, it } from "vitest";
 
+const sortObj = (obj) => {
+	if (typeof obj === "object") {
+		const right = obj?.right;
+		const left = obj?.left
+		const value = obj?.value
+
+		let rightCopy = right
+		let leftCopy = left
+
+		if (right instanceof Object) rightCopy = sortObj(right)
+
+		if (left instanceof Object) leftCopy = sortObj(left)
+
+		return {
+			value,
+			right: rightCopy,
+			left: leftCopy
+		}
+
+	} else {
+		return obj
+	}
+}
+
 const checkIsSameTree = (treeA, treeB) => {
 	const isInvalid = !treeA || typeof treeA !== "object"
 		|| treeA instanceof Object === false
@@ -7,17 +31,31 @@ const checkIsSameTree = (treeA, treeB) => {
 		|| treeB instanceof Object === false
 	if (isInvalid) throw new Error()
 
-	return false
+	treeA = sortObj(treeA)
+	treeB = sortObj(treeB)
+
+	const strA = JSON.stringify(treeA)
+	const strB = JSON.stringify(treeB)
+
+	const areEqual = strA === strB;
+
+	return areEqual
 }
 
 describe("checkIsSameTree", () => {
-	it("Should be a function", () => {
-		expect(checkIsSameTree).toBeTypeOf("function")
-	})
+	const tree = {
+		value: 1,
+		left: { value: 2, left: null, right: null },
+		right: { value: 3, left: null, right: null }
+	}
 
-	it("Should throw error if no parameter provided", () => {
-		expect(() => checkIsSameTree()).toThrow()
-	})
+	// it("Should be a function", () => {
+	// 	expect(checkIsSameTree).toBeTypeOf("function")
+	// })
+
+	// it("Should throw error if no parameter provided", () => {
+	// 	expect(() => checkIsSameTree()).toThrow()
+	// })
 
 	it("Should throw error if first parameter is not an object", () => {
 		expect(() => checkIsSameTree(Number, Object)).toThrow()
@@ -37,5 +75,19 @@ describe("checkIsSameTree", () => {
 
 	it("Should return a boolean", () => {
 		expect(checkIsSameTree({}, {})).toBeTypeOf("boolean")
+	})
+
+	it("Should return true if both trees are equal", () => {
+		expect(checkIsSameTree(tree, tree)).toBe(true)
+	})
+
+	it("Should be able to notice when the trees are not the same", () => {
+		const tree2 = {
+			value: 1,
+			left: { value: 3, left: { value: 2, left: null, right: null }, right: null },
+			right: { value: 5, left: null, right: { value: 4, left: null, right: null } }
+		}
+
+		expect(checkIsSameTree(tree, tree2)).toBe(false)
 	})
 })
